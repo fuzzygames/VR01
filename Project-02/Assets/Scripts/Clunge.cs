42fe6bf5a15b8a96c0985fb3c44551a09d66a834
fuzzygames <fuzzywobs@gmail.com>
2015-04-12 01:52:16 +0100

    Sat night update
    Changed the security room around a bit.

---
+++ Project-02/Assets/Scripts/Clunge.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Clunge : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	//this is a programme
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
